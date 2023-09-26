32fd2466e92207a72cddddc0fe1c513e67345a9f
Dawid Loranc <dawidloranc@gmail.com>
2023-09-26 15:00:56 +0200

    Use 01-run.sh

--- my-stage/00-install-app/01-run.sh
+++ my-stage/00-install-app/01-run.sh
@@ -11,12 +11,13 @@ ls /home
 
 echo "Find run.sh file"
 find / -type f -name "run.sh" 2>/dev/null && echo "File found." || echo "File not found or an error occurred."
+find / -type f -name "run.sh"
 
 ##############################
 
-install -m 644 files/hello.cpp "${ROOTFS_DIR}/home/test-user"
+# install -m 644 files/hello.cpp "${ROOTFS_DIR}/home/test-user"
 
-cd /home/test-user
+# cd /home/test-user
 
-g++ -o hello hello.cpp
-rm hello.cpp
+# g++ -o hello hello.cpp
+# rm hello.cpp
