f01410b86be48e98c1f7008642e3ac70f497564c
Dawid Loranc <dawidloranc@gmail.com>
2023-09-26 13:28:46 +0200

    Add some echos

--- my-stage/00-install-app/00-run-chroot.sh
+++ my-stage/00-install-app/00-run-chroot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/test-user
 cat > hello.cpp <<-EOF2
 #include <iostream>
 
@@ -6,8 +7,14 @@ int main() {
   std::cout << "Hello, World!" << std::endl;
 }
 EOF2
-ls /
-ls /home
 g++ -o hello hello.cpp
 rm hello.cpp
-find / -type f -name "run.sh" 2>/dev/null
+
+echo "ls /"
+ls /
+
+echo "ls /home"
+ls /home
+
+echo "Find run.sh file"
+find / -type f -name "run.sh" 2>/dev/null && echo "File found." || echo "File not found or an error occurred."
